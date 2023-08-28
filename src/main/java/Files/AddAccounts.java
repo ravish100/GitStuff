96497f77c062dcae98c7c2f86bf138ac375d49f5
Ravish <malik.ravish@gmail.com>
2023-08-27 23:44:17 -0500

    my first commit

--- src/main/java/Files/AddAccounts.java
+++ src/main/java/Files/AddAccounts.java
@@ -5,5 +5,6 @@ public class AddAccounts {
 	{
 		return "{ \"uid\": \""+NAME+"\", \"accounts\": [\"000078\",\"096206\",\"241654\",\"553183\",\"283044\",\"878586\"]}";
 	}
-
+	System.out.println("update part1 on 08/27");
+	System.out.println("update part2 on 08/27");
 }
